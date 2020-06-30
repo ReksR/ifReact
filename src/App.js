efd46a2369356e400f0aae43cd51bc11f3963a28
devopsrekha <61942349+devopsrekha@users.noreply.github.com>
2020-06-30 10:18:59 +0530

    with style

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React from "react"
+import React from "react";
 
 function App() {
-const firstNum = 10
-let final
-if ((firstNum < 20) ||  (firstNum > 30)) {
-  final = "Small"
-} else {
-  final ="none"
+  const firstNum = 10;
+  let final;
+  if (firstNum < 20 || firstNum > 30) {
+    final = "Small";
+  } else {
+    final = "none";
+  }
+  const sty = {
+    color: "purple",
+    backgroundColor: "yellow",
+    fontSize: "50px"
+  };
+  return <h1 style={sty}> the number is {final}</h1>;
 }
-
-  return(
-    <h1> the number is {final}</h1>
-  )
-}
-export default App
+export default App;
